1286931130782f0419a52973bf03f2fa9c011271
Anuhs <color59abe@gmail.com>
2020-10-12 22:55:16 +0900

    文字列の配列もソートできるようにしました

--- sort/merge_sort.f90
+++ sort/merge_sort.f90
@@ -5,7 +5,7 @@ module merge_sort_mod
     public:: merge_sort
 
     interface merge_sort
-        module procedure ms32, ms64
+        module procedure ms32, ms64, msc
     end interface
 contains
     recursive subroutine ms32(ar)
@@ -69,6 +69,43 @@ contains
         do i=1,size(ar)
             if (m < l) then
                 ar(i:)=tmp(r:); return
+            else if (size(ar) < r) then
+                ar(i:)=tmp(l:m); return
+            else
+                if (tmp(l) <= tmp(r)) then
+                    ar(i) = tmp(l); l=l+1
+                else
+                    ar(i) = tmp(r); r=r+1
+                end if
+            end if
+        end do
+    end subroutine
+
+
+    recursive subroutine msc(ar)
+        character(*),intent(inout):: ar(:)
+        integer(int32):: m
+
+        if (size(ar) <= 1) then
+            return
+        else
+            m = size(ar)/2
+            call msc(ar(:m)); call msc(ar(m+1:))
+            call sub_msc(ar)
+        end if
+    end subroutine
+
+
+    subroutine sub_msc(ar)
+        character(*),intent(inout):: ar(:)
+        character(:),allocatable:: tmp(:)
+        integer(int32):: m,l,r,i
+        
+        allocate(tmp, source=ar)
+        m = size(ar)/2; l=1;r=m+1
+        do i=1,size(ar)
+            if (m < l) then
+                ar(i:)=tmp(r:); return 
             else if (size(ar) < r) then
                 ar(i:)=tmp(l:m); return
             else
--- string.f90
+++ string.f90
@@ -3,36 +3,36 @@ module string_mod
     public
     integer(int32),private:: d = ichar("a") - ichar("A")
 contains
-function toupper(s) result(big_s)
-    character(*),intent(in):: s
-    character(1):: c(len_trim(s))
-    character(len_trim(s)):: big_s
+    function toupper(s) result(big_s)
+        character(*),intent(in):: s
+        character(1):: c(len_trim(s))
+        character(len_trim(s)):: big_s
 
-    c = transfer(trim(s),c)
-    where("a"<=c .and. c<="z") c = char(ichar(c)-d)
-    big_s = transfer(c,repeat(" ", size(c)))
-end function
-
-
-function tolower(s) result(big_s)
-    character(*),intent(in):: s
-    character(1):: c(len_trim(s))
-    character(len_trim(s)):: big_s
-
-    c = transfer(trim(s),c)
-    where("A"<=c .and. c<="Z") c = char(ichar(c)+d)
-    big_s = transfer(c,repeat(" ", size(c)))
-end function
+        c = transfer(trim(s),c)
+        where("a"<=c .and. c<="z") c = char(ichar(c)-d)
+        big_s = transfer(c,repeat(" ", size(c)))
+    end function
 
 
-function read_grid(w,h) result(s)
-    ! how to use => "s = read_grid(w,h)"
-    integer(int32),intent(in):: w,h
-    character(1):: s(w,h)
-    character(w):: lines(h)
-    integer(int32):: i
+    function tolower(s) result(big_s)
+        character(*),intent(in):: s
+        character(1):: c(len_trim(s))
+        character(len_trim(s)):: big_s
 
-        read*, (lines(i), i=1,h)
-        s = reshape([(transfer(lines(i),s(:,i)), i=1,h)], [w,h])
-end function
+        c = transfer(trim(s),c)
+        where("A"<=c .and. c<="Z") c = char(ichar(c)+d)
+        big_s = transfer(c,repeat(" ", size(c)))
+    end function
+
+
+    function read_grid(w,h) result(s)
+        ! how to use => "s = read_grid(w,h)"
+        integer(int32),intent(in):: w,h
+        character(1):: s(w,h)
+        character(w):: lines(h)
+        integer(int32):: i
+
+            read*, (lines(i), i=1,h)
+            s = reshape([(transfer(lines(i),s(:,i)), i=1,h)], [w,h])
+    end function
 end module
