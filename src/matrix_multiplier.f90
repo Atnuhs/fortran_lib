de6b450566df5d6bf213ef3dc25e4eb19dece16d
Atnuhs <color59abe@gmail.com>
2021-05-09 20:54:24 +0900

    add mutmulmod

--- src/matrix_multiplier.f90
+++ src/matrix_multiplier.f90
@@ -2,13 +2,33 @@ module matrix_multiplier_mod
     use,intrinsic :: iso_fortran_env
     implicit none
 contains
+    function matmul_mod(a,b,md) result(ret)
+        integer(int64),intent(in):: a(:,:),b(:,:),md
+        integer(int64),allocatable:: ret(:,:)
+        integer(int32):: i,j,k,ni,nj,nk
+ 
+        ni = size(a(1,:))
+        nj = size(b(:,1))
+        nk = size(a(:,1))
+        allocate(ret(ni,nj), source=0_int64)
+ 
+        do i=1,ni
+            do j=1,nj
+                do k=1,nk
+                    ret(j,i) = mod(ret(j,i)+mod(a(k,i)*b(j,k),md),md)
+                end do
+            end do
+        end do
+    end function
+ 
+ 
     function matmuler(a,e,md) result(ret)
         integer(int64),intent(in):: a(:,:),md
         integer(int64),value:: e
         integer(int64), allocatable:: ma(:,:), ret(:,:)
         integer(int32):: i
-
-
+ 
+ 
         allocate(ma, source=a)
         allocate(ret, mold=a)
         ret(:,:) = 0
@@ -17,14 +37,10 @@ contains
         end do
         do while(e>0)
             if (btest(e,0)) then
-                ret = matmul(ret, ma)
+                ret = matmul_mod(ret, ma, md)
             end if
-            print*, '---',e
             e = shiftr(e,1)
-            ma = matmul(ma,ma)
-            do i=1,size(ma(:,1))
-                print'(*(i0,1x))', ma(:,i)
-            end do
+            ma = matmul_mod(ma,ma, md)
         end do
     end function
 end module
