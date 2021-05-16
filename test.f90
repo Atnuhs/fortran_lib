5ffccaced75fd606ee64241c05b0f6aa9b5fe55c
Atnuhs <color59abe@gmail.com>
2021-05-16 16:08:19 +0900

    test commit

--- test.f90
+++ test.f90
@@ -2,5 +2,5 @@ program main
     use,intrinsic :: iso_fortran_env
     implicit none
     
-    print'(a)', 'This is test program for github actions!!!!!'
+    print'(a)', 'This is test program for github actions!!!!'
 end program main
