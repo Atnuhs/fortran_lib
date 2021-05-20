ebaaa7238de16a5d2e474b05930fc83f76d251e7
Atnuhs <color59abe@gmail.com>
2021-05-20 12:01:21 +0900

    minimum test ok

--- test/test_random/test_random.f90
+++ test/test_random/test_random.f90
@@ -1,20 +1,32 @@
-program main
+module test_random_mod
     use,intrinsic :: iso_fortran_env
-    use random_mod
+    use,intrinsic:: iso_c_binding
     implicit none
-    integer(int32):: n,i,imin,imax,amax,amin
-    integer(int32), allocatable:: a(:)
+    private
+contains
+    function resultval(flg) result(ret)
+        integer(int32):: ret
+        logical,intent(in):: flg
 
-    n=10000
-    imin=0
-    imax=100
-    allocate(a(n), source=[(randrange(imin,imax),i=1,n)])
-    amax = a(1)
-    amin = a(1)
-    do i=1,n
-        amax = max(amax, a(i))
-        amin = min(amin, a(i))
-    end do
-    print*, amax <= imax
-    print*, amin >= imin
-end program main
+        ret = merge(0,1,flg)
+    end function
+    function test_random() result(ret) bind(c, name="test_random")
+        use random_mod
+        implicit none
+        integer(int32):: n,i,imin,imax,amax,amin
+        integer(int32), allocatable:: a(:)
+        integer(c_int):: ret
+
+        n=10000
+        imin=0
+        imax=100
+        allocate(a(n), source=[(randrange(imin,imax),i=1,n)])
+        amax = a(1)
+        amin = a(1)
+        do i=1,n
+            amax = max(amax, a(i))
+            amin = min(amin, a(i))
+        end do
+        ret = resultval(amax <= imax .and. amin >= imin)
+    end function
+end module
