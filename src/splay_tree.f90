73b04692d3fe42ad094c3ddbb6f6f39c9718de4b
Atnuhs <color59abe@gmail.com>
2021-06-27 22:12:30 +0900

    splaytree作成

--- src/splay_tree.f90
+++ src/splay_tree.f90
@@ -4,25 +4,22 @@ module splay_tree_mod
     use,intrinsic :: iso_fortran_env
     use splay_node_mod
 contains
-    
-
-
     subroutine st_merge(lroot, rroot)
         type(splay_node),pointer:: lroot, rroot
         ! lrootにrrootをくっつける
-
+        ! 片方空っぽの時のための例外処理
         if (.not. associated(lroot))then
             lroot = rroot
         else if (.not. associated(rroot))then
             lroot = lroot
             return
-        else
+        else ! 両方あるとき 
             ! lrootの右端をrootに
-            call st_rooting(lroot, lroot%size)
+            call sn_rooting(lroot, lroot%size)
             ! rrootをlrootの右の子にする
             lroot%right = rroot
             rroot%parent = lroot
-            call lroot%size_update()
+            call sn_size_update(lroot)
         end if
     end subroutine
 
@@ -32,31 +29,49 @@ contains
         integer(int32), intent(in):: left_cnt
 
         if (left_cnt == 0) then
+            ! 左のサイズが0
             lroot => null()
             rroot = root
         else if (left_cnt == root%size) then
+            ! 左のサイズが全部
             lroot = root
             rroot => null()
         else
-            call st_rooting(root, left_cnt)
+            call sn_rooting(root, left_cnt)
             lroot = root
             rroot = root%right
             lroot%right => null()
             rroot%parent => null()
-            call lroot%size_update()
+            call sn_size_update(lroot)
         end if
     end subroutine
 
 
     subroutine st_insert(root, ind, node)
-        class(splay_node):: root, node
+        type(splay_node),pointer:: root, node
+        type(splay_node),pointer:: lroot, rroot
         integer(int32),intent(in):: ind
 
+        call st_split(root, ind-1, lroot, rroot)
+        call st_merge(lroot, node)
+        call st_merge(lroot, rroot)
+        root => lroot
     end subroutine
 
 
     subroutine st_delete(root, ind)
-        class(splay_node):: root
+        type(splay_node),pointer:: root
+        type(splay_node),pointer:: lroot, rroot
         integer(int32),intent(in):: ind
-end subroutine
+
+        call sn_rooting(root, ind)
+        lroot => root%left
+        rroot => root%right
+        if (associated(lroot)) lroot%parent => null()
+        if (associated(rroot)) rroot%parent => null()
+        deallocate(root)
+        call st_merge(lroot, rroot)
+        root => lroot
+        call sn_size_update(root)
+    end subroutine
 end module
