2d8e8232087b9411dd997376d1d75fff84485f88
Atnuhs <color59abe@gmail.com>
2021-06-27 17:17:05 +0900

    　新しい関数の外装だけ作成

--- src/splay_tree.f90
+++ src/splay_tree.f90
@@ -7,14 +7,11 @@ module splay_node_mod
         type(splay_node),pointer:: right => null()
         type(splay_node),pointer:: parent => null()
         integer(int32):: size = 1
-        integer(prec):: value
+        integer(prec):: value, minimum
     contains
-        procedure:: has_left => sn_has_left
-        procedure:: has_right => sn_has_right
-        procedure:: has_parent => sn_has_parent
         procedure:: state => sn_state
         procedure:: rotate => sn_rotate
-        procedure:: update => sn_update
+        procedure:: size_update => sn_size_update
         procedure:: splay => sn_splay
         procedure,private,pass:: sn_equals
         procedure,private:: sn_assign
@@ -48,30 +45,6 @@ contains
     end subroutine
 
 
-    function sn_has_left(sn)
-        class(splay_node),intent(in):: sn
-        logical:: sn_has_left
-
-        sn_has_left = associated(sn%left)
-    end function
-
-
-    function sn_has_right(sn)
-        class(splay_node),intent(in):: sn
-        logical:: sn_has_right
-
-        sn_has_right = associated(sn%right)
-    end function
-
-
-    function sn_has_parent(sn)
-        class(splay_node),intent(in):: sn
-        logical:: sn_has_parent
-
-        sn_has_parent = associated(sn%parent)
-    end function
-
-
     function sn_state(sn)
         class(splay_node),intent(in):: sn
         integer(int32):: sn_state
@@ -88,14 +61,20 @@ contains
         class(splay_node),intent(inout):: sn
         type(splay_node),pointer:: pp, p, c
 
-        p = sn%parent
-        pp = pp%parent
+        ! 登場人物:: 自分(sn), 親(p), 親親(pp), 子供(c)
+        ! 自分と親の存在は確定。親親の存在は未定
 
-        if (p%left == sn) then
+        ! 自分=子供の片方, 自分=親, 親=親親 それぞれ双方向
+        ! 計6本の辺を張り替える。
+
+        p = sn%parent
+        pp = p%parent
+
+        if (p%left == sn) then ! 左回転
             c = sn%right
             sn%right = p
             p%left = c
-        else
+        else ! 右回転
             c = sn%left
             sn%left = p
             p%right = c
@@ -105,25 +84,31 @@ contains
             if (pp%left == p) pp%left = sn
             if (pp%right == p) pp%right = sn
         end if
+        
         sn%parent = pp
         p%parent = sn
-        c%parent = p
+        if (associated(c)) c%parent = p
 
-        call p%update()
-        call sn%update()
+        ! 回転後は自分と親のサイズが変わっている。
+        ! 回転後は自分は親より上にいるので、親->自分の順にsize_update
+        call p%size_update()
+        call sn%size_update()
     end subroutine
 
 
     subroutine sn_splay(sn)
         class(splay_node):: sn
 
-        do while(sn%has_parent())
-            if (sn%parent%state() == 0) then ! 親の親がいない
+        ! 自分の親がいなくなるまで(=自分が根になるまで)続ける
+        do while(associated(sn%parent))
+            if (sn%parent%state() == 0) then 
+                ! 親の親がいない
                 call sn%rotate()
-            else if (sn%state() == sn%parent%state()) then ! me->p->ppが直線
+            else if (sn%state() == sn%parent%state()) then 
+                ! me->p->ppが直線 => 親->自分の順にrotate
                 call sn%parent%rotate()
                 call sn%rotate()
-            else ! me->p->ppが折れ線
+            else ! me->p->ppが折れ線 => 自分を2回rotate
                 call sn%rotate()
                 call sn%rotate()
             end if
@@ -131,31 +116,44 @@ contains
     end subroutine
 
 
-    subroutine sn_update(sn)
+    subroutine sn_size_update(sn)
         class(splay_node):: sn
 
         sn%size = 1
-        if (sn%has_left()) sn%size = sn%size+sn%left%size
-        if (sn%has_right()) sn%size = sn%size+sn%right%size
+        sn%minimum = sn%value 
+        if (associated(sn%left)) then
+            sn%size = sn%size+sn%left%size
+            sn%minimum = min(sn%minimum, sn%left%minimum)
+        end if
+        if (associated(sn%right)) then
+            sn%size = sn%size+sn%right%size
+            sn%minimum = min(sn%minimum, sn%right%minimum)
+        end if
     end subroutine
 
 
-    function sn_get(sn, ind)
+    function sn_get_node(sn, ind) result(ret_node)
         class(splay_node),intent(in):: sn
-        type(splay_node):: now, sn_get
+        type(splay_node):: now, ret_node
         integer(prec),value:: ind
         integer(int32):: now_ind
 
         now = sn
+        ! ind番目のノードを探しに、根から降りていく
+        ! nowは今いるノード, now_indは今いるノードが何番目のノードか。
         do while(.true.)
-            now_ind = 0
-            if (now%has_left()) now_ind = sn%left%size + 1
+            if (associated(now%left))then
+                now_ind = sn%left%size + 1
+            else
+                now_ind = 1
+            end if
+
             if (ind < now_ind) then
                 now = now%left
             end if
             if (ind == now_ind) then
                 call now%splay()
-                sn_get = now
+                ret_node = now
             end if
             if (ind > now_ind) then
                 now = now%right
@@ -163,4 +161,34 @@ contains
             end if
         end do
     end function
+
+
+    subroutine sn_merge(lroot, rroot)
+        class(splay_node):: lroot
+        type(splay_node):: rroot
+        ! 左のノードに右のノードをくっつける
+
+
+    end subroutine
+
+
+    subroutine sn_split(sn, left_cnt, lroot, rroot)
+        class(splay_node):: sn
+        type(splay_node),intent(out):: lroot, rroot
+        integer(int32),intent(in):: left_cnt
+
+    end subroutine
+
+
+    subroutine sn_insert(sn, ind, node)
+        class(splay_node):: sn, node
+        integer(int32),intent(in):: ind
+
+    end subroutine
+
+
+    subroutine sn_delete(sn, ind)
+        class(splay_node):: sn
+        integer(int32),intent(in):: ind
+end subroutine
 end module
