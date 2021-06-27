e964f7d6f67f67efb5d246d4e86cb685d6eab68b
Atnuhs <color59abe@gmail.com>
2021-06-27 19:20:50 +0900

    ポインタ練習

--- src/sandbox.f90
+++ src/sandbox.f90
@@ -1,54 +1,32 @@
-module a_mod
-    use,intrinsic :: iso_fortran_env
+program linked_list
     implicit none
-    type,abstract:: vec_base
-        integer(int32):: l=0
-    end type
-    type, public, extends(vec_base):: vec_int32
-        integer(int32):: l=0
-        integer(int32),allocatable:: ar(:)
-    contains
-        procedure:: push_back
-    end type
-
-    type, public:: vec_int64
-        integer(int32):: l=0
-        integer(int64),allocatable:: ar(:)
-    contains
-        procedure:: push_back
-    end type
-
-    interface
-        module subroutine push_back(vec,x)
-            class(vec_base):: vec
-            integer(int32):: x
-            integer(int32):: l
-
-        end subroutine
-
-        module subroutine push_back(vec,x)
-            class(vec_base):: vec
-            integer(int64):: x
-            integer(int32):: l
-
-        end subroutine
-    end interface
-end module
-
-submodule(a_mod) a_submodule
-contains
-    module procedure push_back_int32
-        l=l+1
-        vec%ar(l) = x
-    end procedure
-end submodule
-
-
-
-
-program main
-    use,intrinsic :: iso_fortran_env
-    use a_mod
-    implicit none
-    
-end program main
+    type node
+      type (node), pointer :: next_node
+      integer value
+    end type node
+    integer num
+    type (node), pointer :: first_node, new_node, p
+  
+    nullify (first_node)
+  
+    do
+      print *, 'Input a positive number: (Enter negative number to end)'
+      read *, num
+      if (num<0) exit
+  
+      ! リストの最初に新しい値を追加
+      allocate (new_node)
+      new_node%value = num
+      first_node = new_node
+      
+  
+    end do
+  
+    p => first_node
+    print *, "================================"
+    do while (associated(p))
+      print *, p%value
+      p => p%next_node
+    end do
+  
+  end program linked_list
